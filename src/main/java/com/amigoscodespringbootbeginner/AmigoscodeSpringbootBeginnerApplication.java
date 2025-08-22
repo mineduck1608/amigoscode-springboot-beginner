79237ccbc558a6f41a13289cd8f8d7316c56f3a3
mineduck1608 <ducdm160804@gmail.com>
2025-08-22 23:37:01 +0700

    init project

---
+++ src/main/java/com/amigoscodespringbootbeginner/AmigoscodeSpringbootBeginnerApplication.java
@@ -0,0 +1,13 @@
+package com.amigoscodespringbootbeginner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigoscodeSpringbootBeginnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmigoscodeSpringbootBeginnerApplication.class, args);
+    }
+
+}
